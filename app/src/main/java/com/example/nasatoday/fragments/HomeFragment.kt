aca5b93b083eccaf34d0d595f842420a92583a07
Алексей Рогатин <bamako99@mail.ru>
2021-04-18 19:59:10 +0300

    Сделано все ДЗ, кроме чипов

--- app/src/main/java/com/example/nasatoday/fragments/HomeFragment.kt
+++ app/src/main/java/com/example/nasatoday/fragments/HomeFragment.kt
@@ -1,7 +1,6 @@
 package com.example.nasatoday.fragments
 
 import android.os.Bundle
-import android.transition.TransitionInflater
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
